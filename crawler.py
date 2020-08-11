4295ced86d102d1558a07fe11e89dcd6357f074f
Your Name <you@example.com>
2020-08-11 23:02:56 +0800

    新增log、重要欄位若為N/A則不儲存

--- crawler.py
+++ crawler.py
@@ -1,9 +1,13 @@
 from tool.bggapi import BGGAPI
 import csv
 from datetime import datetime
+import logging
+from logging.handlers import TimedRotatingFileHandler
 
-main = 'https://boardgamegeek.com/browse/boardgame/page'
+url = 'https://boardgamegeek.com/browse/boardgame/page'
 
+csvfile = None
+writer = None
 
 def store(result, cnt):
     data_fields, data = list(zip(*list(result.items())))
@@ -12,12 +16,40 @@ def store(result, cnt):
 
     writer.writerow(data)
 
-now = datetime.now()
-date_time = now.strftime('%Y%m%d')
-csvfile = open('{0}.csv'.format(date_time), 'a')
-writer = csv.writer(csvfile)
+def set_logger():
+    logging.basicConfig(filename='{0}/{1}.log'.format('./logs', 'main'), format='%(asctime)s - %(levelname)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
 
-api = BGGAPI()
-print("共蒐集到 {0} 筆遊戲, 最後一頁為 {1}".format(*api.get_rank_list(main, 1, 2, store, 1 )))
+    # logger = logging.getLogger("main")
+    # fhandle = TimedRotatingFileHandler(
+    #     '{0}/{1}.log'.format('./logs', 'main'), when='D', interval=1, backupCount=3)
+    # fhandle.suffix = '%Y%m%d'
 
-csvfile.close()
+    # formatter = logging.Formatter(
+    #     '%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    # fhandle.setFormatter(formatter)
+    # fhandle.setLevel(logging.DEBUG)
+
+    # logger.addHandler(fhandle)
+    # return logger
+    return
+
+
+
+def main():
+    set_logger()
+
+    api = BGGAPI()
+    logging.info("共蒐集到 {0} 筆遊戲, 最後一頁為 {1}".format(
+        *api.get_rank_list(mainurl=url, startpage=93, endpage=94, store=store, interval=1)))
+
+    if csvfile:
+        csvfile.close()
+
+if __name__ == '__main__':
+    now = datetime.now()
+    date_time = now.strftime('%Y%m%d')
+    csvfile = open('{0}.csv'.format(date_time), 'a')
+    writer = csv.writer(csvfile)
+
+    main()
--- tool/bggapi/ranklist/__init__.py
+++ tool/bggapi/ranklist/__init__.py
@@ -6,6 +6,9 @@ import ssl
 import re
 import json
 import time
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
 
 DEFAULT_NO_VALUE = 'N/A'
 main = 'https://boardgamegeek.com/browse/boardgame/page/'
@@ -18,31 +21,39 @@ def get_rank(root):
     if root.get('class')[0].strip() != "collection_rank":
         return -1
     if not root.find_all('a'):
-        return root.string.strip()
+        return DEFAULT_NO_VALUE
 
     return root.find_all('a')[0].get('name')
 
 def get_image(root):
     if root.get('class')[0].strip() != "collection_thumbnail":
         return -1
-    if not root.find_all('a'):
-        return root.string.strip()
+    if not root.find_all('a') or not root.find_all('a')[0].find_all('img'):
+        return DEFAULT_NO_VALUE
 
     return root.find_all('a')[0].find_all('img')[0].get('src')
+
 def get_bgid_title_and_year(root):
+    bgid = DEFAULT_NO_VALUE
+    title = DEFAULT_NO_VALUE
+    year = DEFAULT_NO_VALUE
+
     if root.get('class')[0].strip() != "collection_objectname":
         return -1
     info = root.find('div', id=re.compile("^results_objectname"))
+    if not info.find_all('a'):
+        return (bgid, title, year)
+
     link_and_title = info.find_all('a')[0]
     link = link_and_title.get('href')
 
-    bgid = re.search(r'/boardgame[\w]*/([\d]+)/', link).group(1)
+    groups = re.search(r'/boardgame[\w]*/([\d]+)/', link)
+    if groups:
+        bgid = groups.group(1)
 
     title = link_and_title.string.strip()
 
-    if not info.find_all('span'):
-        year = DEFAULT_NO_VALUE
-    else:
+    if info.find_all('span'):
         year = info.find_all('span')[0].string
         p = re.compile('[()]')
         year = p.sub('', year).strip()
@@ -68,6 +79,18 @@ def del_nonused_fields(data):
     del data['image']
     del data['others']
 
+
+def detect_fields(data):
+    flag = True
+    err_fields = []
+    check_fields = ['bgid', 'title', 'year']
+    for field in check_fields:
+        if data[field] == DEFAULT_NO_VALUE:
+            flag = False
+            err_fields.append(field)
+
+    return (flag, err_fields)
+
 def parse_fields(root, game_index, page):
     result_values = []
     # get_bgid_title_and_year拿到tuple結果會拆成三個欄位儲存: 'bgid', 'title', 'year'
@@ -91,8 +114,8 @@ def parse_fields(root, game_index, page):
         value = field_extract[fields[field_index]](td)
 
         if value == -1 or value == None: # -1代表欄位的DOM不符合預期
-            raise SyntaxError("Page {page}, NO. {rank} , Field {field_index} 不符合預期格式: {fname}".format(page=page,
-                rank=game_index, field_index=field_index+1, fname=field_extract[field_index]))
+            raise SyntaxError("Page {page}, NO. {game_index}, Field {fname}({field_index}) 不符合預期格式".format(page=page,
+                                                                                                           game_index=game_index, field_index=field_index+1, fname=fields[field_index]))
         else:
             if type(value) == tuple:
                 result_values.extend(value)
@@ -101,15 +124,21 @@ def parse_fields(root, game_index, page):
         field_index += 1
     result = dict(zip(result_fields, result_values))
     del_nonused_fields(result)
+
+    ok, err_fields = detect_fields(result)
+    if not ok:
+        raise SyntaxError("Page {page}, NO. {game_index}({bgid}), Fields {fields} 值為 {default}".format(page=page, game_index=game_index, bgid=result['bgid'], fields=','.join(err_fields), default=DEFAULT_NO_VALUE))
+
     return result
 
-def has_nextpage(root):
+def has_nextpage(root, page):
     try:
         next_page = root.find('a', title='next page').get('href')
     except AttributeError:
+        logging.error('第 {0} 頁的next page link 格式不符合預期'.format(page))
         return False
     else:
-        #print(next_page)
+        logging.debug(next_page)
         return True
 
 def default_store(result, cnt):
@@ -124,27 +153,34 @@ def get(main, page=1, limit=-1, store=default_store, interval=10, cnt=0):
             data = fhand.read()
             soup = BeautifulSoup(data, 'html.parser')
     except HTTPError as e:
-        print("{code}: {reason}({url})".format(code=e.code, reason=e.reason, url=url))
+        logging.error("{code}: {reason}({url})".format(
+            code=e.code, reason=e.reason, url=url))
         return (cnt, page-1)
     except URLError as e:
-        print(e.reason)
+        logging.error(e.reason)
         return (cnt, page-1)
     except:
-        print(sys.exc_info())
+        logging.error(sys.exc_info())
         return (cnt, page-1)
     else:
         game_index = 0 # 遊戲在該頁的第幾位
         # 最頂端為欄位文字
         for tr in soup.find_all('tr')[1:]:
-            store(parse_fields(tr, game_index, page), cnt)
-            cnt += 1
+            try:
+                result = parse_fields(tr, game_index, page)
+            except SyntaxError as e:
+                logging.error(e.msg)
+            else:
+                store(result, cnt)
+                cnt += 1
+
             game_index += 1
 
         page += 1
         if limit != -1 and page > limit:
             return (cnt, page-1)
 
-        if has_nextpage(soup):
+        if has_nextpage(soup, page):
             time.sleep(interval)
             return get(main, page, limit, store, interval, cnt)
         else:
