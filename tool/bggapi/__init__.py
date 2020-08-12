2824b6e8edc3236e738a37ba33643a918b81e6f2
Your Name <you@example.com>
2020-08-12 18:05:45 +0800

    由於環境變數預設值無法將float inf轉為int，所以-1先暫時代表無限，在這邊才轉成inf

--- tool/bggapi/__init__.py
+++ tool/bggapi/__init__.py
@@ -9,7 +9,7 @@ class BGGAPI:
         """Constructor
         """
 
-    def get_rank_list(self, mainurl=None, startpage=1, endpage=float('Inf'), store=None, interval=10):
+    def get_rank_list(self, mainurl=None, startpage=1, endpage=1, store=None, interval=10):
         """
         :param mainurl: bgg排名頁面url
         :param startpage: 從哪一頁開始蒐集，預設從第1頁
@@ -18,4 +18,8 @@ class BGGAPI:
         :param interval: 每幾秒發一次request，預設10秒
         :return: (cnt, page) => (總共蒐集到的遊戲數量, 蒐集到第幾頁)
         """
+        # 由於環境變數預設值無法將float inf轉為int，所以-1先暫時代表無限，在這邊才轉成inf
+        if endpage == -1:
+            endpage = float('Inf')
+
         return ranklist.get(mainurl, startpage, endpage, store, interval)
