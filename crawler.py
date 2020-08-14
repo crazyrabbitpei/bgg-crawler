80732cc59b13b15bd709efe41bf1d9d8aad54ec3
Your Name <you@example.com>
2020-08-14 20:16:10 +0800

    排版

--- crawler.py
+++ crawler.py
@@ -13,8 +13,8 @@ RANK_URL = 'https://boardgamegeek.com/browse/boardgame/page'
 
 def main():
     crawler = BggCralwer(store_config={'store_mode': 'file', 'data_type': 'csv', 'data_path': './data', 'data_name': 'rank_list'})
-    logger.info("共蒐集到 {0} 筆遊戲, 最後一頁為 {1}".format(
-        *crawler.get_rank_list(mainurl=RANK_URL, startpage=int(os.getenv('RANK_PAGE_FROM', 1)), endpage=int(os.getenv('RANK_PAGE_TO', 1)), interval=int(os.getenv('INTERVAL', 1)))))
+    logger.info("共蒐集到 {0} 筆遊戲, 最後一頁為 {1}".format( *crawler.get_rank_list(mainurl=RANK_URL, startpage=int(os.getenv('RANK_PAGE_FROM', 1)), endpage=int(os.getenv('RANK_PAGE_TO', 1)), interval=int(os.getenv('INTERVAL', 1)))))
+
 
 if __name__ == '__main__':
     main()
